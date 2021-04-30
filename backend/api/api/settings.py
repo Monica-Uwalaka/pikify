c6e395407905a089761377e9d8fb484ce015a0de
Monica Uwalaka <muwalaka@ualberta.ca>
2021-04-30 13:45:08 -0600

    connected to postgreSQL database

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pikify_database',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
